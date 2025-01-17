7e84db93bb7ca827a2af9e1f253dd534894b031d
{ibrahim-srihi-vinci} <ibrahim.srihi@student.vinci.be>
2025-01-17 17:17:12 +0100

    Format code with Prettier

--- src/services/passwordChecker.js
+++ src/services/passwordChecker.js
@@ -1,10 +1,10 @@
 function validatePassword(password) {
-    const hasMinLength = password.length >= 8;
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    const hasDigit = /\d/.test(password);
-    const hasNoIPL = !/ipl/i.test(password);
-    
-    return hasMinLength && hasSpecialChar && hasDigit && hasNoIPL;
+  const hasMinLength = password.length >= 8;
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const hasDigit = /\d/.test(password);
+  const hasNoIPL = !/ipl/i.test(password);
+
+  return hasMinLength && hasSpecialChar && hasDigit && hasNoIPL;
 }
 
 module.exports = validatePassword;
--- src/tests/passwordChecker.test.js
+++ src/tests/passwordChecker.test.js
@@ -1,21 +1,21 @@
 const validatePassword = require('../services/passwordChecker');
 
 test('valid password', () => {
-    expect(validatePassword('Valid@123')).toBe(true);
+  expect(validatePassword('Valid@123')).toBe(true);
 });
 
 test('password too short', () => {
-    expect(validatePassword('Val@1')).toBe(false);
+  expect(validatePassword('Val@1')).toBe(false);
 });
 
 test('missing special character', () => {
-    expect(validatePassword('Valid1234')).toBe(false);
+  expect(validatePassword('Valid1234')).toBe(false);
 });
 
 test('missing digit', () => {
-    expect(validatePassword('Valid@word')).toBe(false);
+  expect(validatePassword('Valid@word')).toBe(false);
 });
 
 test('contains IPL', () => {
-    expect(validatePassword('IPL@1234')).toBe(false);
+  expect(validatePassword('IPL@1234')).toBe(false);
 });
