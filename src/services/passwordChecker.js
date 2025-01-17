8418bed4f37173a0207c5aeaef27843b3003fd3c
{ibrahim-srihi-vinci} <ibrahim.srihi@student.vinci.be>
2025-01-17 17:03:53 +0100

    Update: password validation function to exclude IPL

--- src/services/passwordChecker.js
+++ src/services/passwordChecker.js
@@ -2,7 +2,9 @@ function validatePassword(password) {
     const hasMinLength = password.length >= 8;
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     const hasDigit = /\d/.test(password);
-    return hasMinLength && hasSpecialChar && hasDigit;
+    const hasNoIPL = !/ipl/i.test(password);
+    
+    return hasMinLength && hasSpecialChar && hasDigit && hasNoIPL;
 }
 
 module.exports = validatePassword;
