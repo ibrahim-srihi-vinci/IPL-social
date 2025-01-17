d41650d844c80a8d645fd7a08409e45ed630da91
{ibrahim-srihi-vinci} <ibrahim.srihi@student.vinci.be>
2025-01-17 16:57:18 +0100

    Add: test for password too short

--- src/tests/passwordChecker.test.js
+++ src/tests/passwordChecker.test.js
@@ -3,3 +3,7 @@ const validatePassword = require('../services/passwordChecker');
 test('valid password', () => {
     expect(validatePassword('Valid@123')).toBe(true);
 });
+
+test('password too short', () => {
+    expect(validatePassword('Val@1')).toBe(false);
+});
